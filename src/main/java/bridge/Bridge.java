7ad0fa1dfe4a5933130f84cc60b44420f07ecef7
kwonkeonhyeong <rjsgud19566@gmail.com>
2023-12-13 15:09:27 +0900

    feat : Bridge createBridge, validateBridgeSize 구현 & feat : BridgeMaker makeBridge 구현

--- src/main/java/bridge/Bridge.java
+++ src/main/java/bridge/Bridge.java
@@ -1,5 +1,25 @@
 package bridge;
 
-public class Bridge {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Bridge {
+    private List<String> bridge;
+    private final BridgeMaker bridgeMaker;
+
+    public Bridge() {
+        bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+    }
+
+    public void createBridge(int size) {
+        validateBridgeSize(size);
+        bridge = bridgeMaker.makeBridge(size);
+    }
+
+    public void validateBridgeSize(int size) {
+        if (size >= 3 && size <= 20) {
+            return;
+        }
+        throw new IllegalArgumentException();
+    }
 }
